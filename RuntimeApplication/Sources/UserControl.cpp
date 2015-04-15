0c738703277e619bcbbd63c54f49f0a988252e0f
Viacheslav Dronov <dronovviacheslav@outlook.com>
2015-04-15 15:43:53 +0300

    Memory view of basePointer

--- RuntimeApplication/Sources/UserControl.cpp
+++ RuntimeApplication/Sources/UserControl.cpp
@@ -1,12 +1,54 @@
 #include "pch.h"
 #include <WeakReference.h>
 #include "UserControl.h"
+#include <typeinfo>
 
 using namespace MTL;
 using namespace MTL::Wrappers;
 using namespace RuntimeApplication;
 using namespace ABI::Windows::UI::Xaml::Controls;
 using namespace ABI::Windows::UI::Xaml;
+
+
+template<typename ... Interfaces>
+struct Tester;
+
+template<typename Head, typename ... Tail>
+struct Tester<Head, Tail...>
+{
+	static void Test(IInspectable* source, RuntimeClassBase<Head, Tail...> * cast)
+	{
+		ComPtr<Head> query;
+		VERIFY_SUCCEEDED(source->QueryInterface(query.GetAddressOf()));
+
+		OutputDebugStringA("\r\n");
+		OutputDebugStringA(typeid(Head).name());
+
+		ASSERT((int)query.Get() == (int)static_cast<Head *>(cast));
+
+		auto innerCast = reinterpret_cast<RuntimeClassBase<Tail...> *>(reinterpret_cast<IInspectable *>(cast) + 1);
+
+		Tester<Tail...>::Test(source, innerCast);
+	}
+};
+
+template<typename End>
+struct Tester<End>
+{
+	static void Test(IInspectable *source, RuntimeClassBase<End> *cast)
+	{
+		ComPtr<End> query;
+		VERIFY_SUCCEEDED(source->QueryInterface(query.GetAddressOf()));
+
+		ASSERT((int)query.Get() == (int)static_cast<End *>(cast));
+	}
+};
+
+template<typename ... Interfaces>
+void TestInterfaces(IInspectable *source, RuntimeClassBase<Interfaces...> *cast)
+{
+	Tester<Interfaces...>::Test(source, cast);
+}
 
 HRESULT RuntimeApplication::UserControl::ActivateInstance(UserControl ** result) noexcept
 {
@@ -23,19 +65,61 @@ HRESULT RuntimeApplication::UserControl::ActivateInstance(UserControl ** result)
 	auto pInnerPointer = (*result)->m_inner.Get();
 	auto pBasePointer = (*result)->m_base.Get();
 
-	ComPtr<IControl2> control;
-	pInnerPointer->QueryInterface(control.GetAddressOf());
+	ASSERT((int)*((IInspectable**)pInnerPointer + 1) == (int)*result);
+	ASSERT((int)(pInnerPointer + 2) == (int)pBasePointer);
 
-	auto cast = reinterpret_cast<RuntimeClassBase<IUserControl, IControl, IControlProtected, IControlOverrides, IControl2, IControl3, IControlExperimental, IContentControlOverrides> *>(pBasePointer);
+	auto cast = reinterpret_cast<RuntimeClassBase<IUserControl,
+		IControl,
+		IControlProtected,
+		IControlOverrides,		
+		IControl2,
+		IControl3,
+		IControlExperimental,
+		IControlOverridesExperimental,
+		IFrameworkElement,
+		IFrameworkElementOverrides,
+		IFrameworkElement2,
+		IFrameworkElementOverrides2,
+		IFrameworkElementExperimental,
+		IUIElement,
+		IUIElementOverrides,
+		IUIElement2,
+		IUIElement3,
+		IUIElementExperimental,
+		IDependencyObject,
+		IDependencyObject2> *>(pBasePointer);
 
-	auto pCastControl = static_cast<IControl2 *>(cast);
+	TestInterfaces(pInnerPointer, cast);
 
-	pCastControl->put_IsTextScaleFactorEnabled(false);
+	//IControlOverridesExperimental test
+	{
+		ComPtr<IControlOverridesExperimental> query;
+		pInnerPointer->QueryInterface(query.GetAddressOf());
 
-	boolean isEnabled = true;
-	control->get_IsTextScaleFactorEnabled(&isEnabled);
-	
-	ASSERT(isEnabled == false);
+		ASSERT((int)query.Get() == (int)static_cast<IControlOverridesExperimental *>(cast));
+	}
+
+	//IUIElementExperimental test
+	{
+		ComPtr<IUIElementExperimental> query;
+		pInnerPointer->QueryInterface(query.GetAddressOf());
+
+		ASSERT((int)query.Get() == (int)static_cast<IUIElementExperimental *>(cast));
+	}
+
+	{
+		ComPtr<IControl2> control;
+		pInnerPointer->QueryInterface(control.GetAddressOf());
+
+		auto pCastControl = static_cast<IControl2 *>(cast);
+
+		pCastControl->put_IsTextScaleFactorEnabled(false);
+
+		boolean isEnabled = true;
+		control->get_IsTextScaleFactorEnabled(&isEnabled);
+
+		ASSERT(isEnabled == false);
+	}
 
 	return S_OK;
 }
